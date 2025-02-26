8d311a3a8e5a1e652eb58fc5d1f3958031028813
AnonLight0o0 <iamreal9123@gmail.com>
2025-02-26 21:00:22 +0800

    added main menu button

---
+++ Assets/Tan/Scripts/MainMenu.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public string sceneToLoad = "MainMenu"; // Assign this in the Inspector
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
